3e54d8bbd40b4d52d52bd6766e9ca154a58f1daf
Lorentz Kim <lorentz@majestika.net>
2011-01-20 02:13:24 +1100

    Bot now understands private message commands and in-channel messages correctly. Refactored code so that additional features can be simply added to Misao class, and uses reflection to execute the right methods.

--- misao-chan.js
+++ misao-chan.js
@@ -9,6 +9,8 @@ var sys = require('sys'),
 
 var irc_instance = new irc(config.irc);
 
+var misao = new Misao();
+
 irc_instance.addListener('privmsg', privmsgListener);
 irc_instance.connect(onConnect);
 
@@ -21,8 +23,53 @@ function onConnect() {
 }
 
 function privmsgListener(message) {
-	if(message.params[0] == config.irc.nick && message.params[1] == 'help') {
+	var to, method, command;
+	if(message.params[0] == config.irc.nick) {
+		// It's in private message
+		to = message.person.nick;
+		method = 'misao.'+message.params[1].toLowerCase().match(/\b[a-z]+/);
+		
+	} else {
+		// General channel message, so we operate a little differently
+		// And yes, I'm aware how the variable names are a clusterfuck
+		
+		to = message.params[0];
+		splitMessage = message.params[1].split(' ');
+		
+		if(splitMessage[0] == config.irc.nick+':') {
+			// Specific to Misao-chan
+			method = 'misao.'+splitMessage[1].toLowerCase();
+		} else {
+			// General chatter
+			method = null;
+		}
+	}
+	command = method+"('"+to+"', '"+message.person.nick+"')";
+	
+	// But calling the method is the same anyway regardless
+	if(method != null && typeof eval(method) == 'function') { 
+		eval(command);
+	} else if (method != null) {
+		misao.methodNotFound(to);
+	}
+}
+
+// Bot itself
+function Misao() {
+	this.help = function(to, username) {
 		msg = 'Please visit "https://github.com/lorentzkim/misao-chan" for usage';
-		irc_instance.privmsg(message.person.nick, msg, true);
+		this.privmsg(to, username, msg);
+	}
+	
+	this.methodNotFound = function(to, username) {
+		msg = 'Invalid command';
+		this.privmsg(to, username, msg);
+	}
+	
+	this.privmsg = function(to, username, msg) {
+		if(to != username) {
+			msg = username+': '+msg;
+		}
+		irc_instance.privmsg(to, msg, true);
 	}
 }
