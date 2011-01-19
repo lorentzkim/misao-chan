2156e69699c6a0e455f32286b7bb4ab13130ff14
Lorentz Kim <lorentz@majestika.net>
2011-01-20 04:38:03 +1100

    Implemented fortune. Refactored code so that user's messages are passed through to Misao's methods. Minimized amount of regex, need to be careful due to usage of eval().

--- misao-chan.js
+++ misao-chan.js
@@ -27,8 +27,8 @@ function privmsgListener(message) {
 	if(message.params[0] == config.irc.nick) {
 		// It's in private message
 		to = message.person.nick;
-		method = 'misao.'+message.params[1].toLowerCase().match(/\b[a-z]+/);
-		
+		splitMessage = message.params[1].split(' ');
+		method = 'misao.'+splitMessage[0].toLowerCase().replace(/[^a-z]/g, '');
 	} else {
 		// General channel message, so we operate a little differently
 		// And yes, I'm aware how the variable names are a clusterfuck
@@ -38,13 +38,20 @@ function privmsgListener(message) {
 		
 		if(splitMessage[0] == config.irc.nick+':') {
 			// Specific to Misao-chan
-			method = 'misao.'+splitMessage[1].toLowerCase();
+			method = 'misao.'+splitMessage[1].toLowerCase().replace(/[^a-z]/g, '');
+			
+			// Done to remove both "bot:" and command itself
+			splitMessage.shift();
+			splitMessage.shift();
 		} else {
 			// General chatter
 			method = null;
 		}
+		
+		
 	}
-	command = method+"('"+to+"', '"+message.person.nick+"')";
+	
+	command = method+"('"+to+"', '"+message.person.nick+"', splitMessage)";
 	
 	// But calling the method is the same anyway regardless
 	if(method != null && typeof eval(method) == 'function') { 
@@ -56,12 +63,25 @@ function privmsgListener(message) {
 
 // Bot itself
 function Misao() {
-	this.help = function(to, username) {
+	
+	this.help = function(to, username, message) {
 		msg = 'Please visit "https://github.com/lorentzkim/misao-chan" for usage';
 		this.privmsg(to, username, msg);
 	}
 	
-	this.methodNotFound = function(to, username) {
+	this.fortune = function(to, username, message) {
+		
+		msgs = [];
+		
+		for(i in config.fortune) {
+			eval(i+' = config.fortune.'+i+'[Math.floor(Math.random()*config.fortune.'+i+'.length)];');
+			msgs.push((i.slice(0,1).toUpperCase() + i.slice(1)) + ': ' + (eval(i)));
+		}
+		
+		this.privmsg(to, username, msgs.join(' | '));
+	}
+	
+	this.methodNotFound = function(to, username, message) {
 		msg = 'Invalid command';
 		this.privmsg(to, username, msg);
 	}
