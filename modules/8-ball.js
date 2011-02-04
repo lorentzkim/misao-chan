00797b0342cce0ebd3595980908636dc8ec908a1
Lorentz Kim <lorentz@majestika.net>
2011-02-04 13:40:45 +1100

    Implemented 8-ball proper.

--- modules/8-ball.js
+++ modules/8-ball.js
@@ -1,9 +1,28 @@
-var util = require('../util.js'),
-	misaoUtil = require('../util.js');;
-
 module.id = '8-ball';
 
 exports.execute = function(msg, callback) {
-	
-	callback(reply);
+	callback(answers[Math.floor(Math.random()*answers.length)]);
 };
+
+var answers = [
+	'As I see it, yes',
+	'It is certain',
+	'It is decidedly so',
+	'Most likely',
+	'Outlook good',
+	'Signs point to yes',
+	'Without a doubt',
+	'Yes',
+	'Yes – definitely',
+	'You may rely on it',
+	'Reply hazy, try again',
+	'Ask again later',
+	'Better not tell you now',
+	'Cannot predict now',
+	'Concentrate and ask again',
+	'Don\'t count on it',
+	'My reply is no',
+	'My sources say no',
+	'Outlook not so good',
+	'Very doubtful'
+];
