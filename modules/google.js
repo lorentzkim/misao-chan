f9b9db3bbdb4e8ab7b522383e059b30c703410de
Link Dupont <link.dupont@me.com>
2011-10-09 18:53:21 -0700

    Module: google
    Build a properly encoded URL

--- modules/google.js
+++ modules/google.js
@@ -3,7 +3,7 @@ var misaoUtil = require('../util.js');
 module.id = 'google';
 
 exports.execute = function(msg, callback) {
-	callback("http://google.com/search?q=" + misaoUtil.stripText(msg));
+	callback(encodeURI("http://google.com/search?q=" + misaoUtil.stripText(msg)));
 };
 
-exports.help = "Constructs a clickable Google search query URL. Usage: google search terms."
+exports.help = "Constructs a clickable Google search query URL. Usage: google <search terms>."
